e0c6f79a0692cdb4bf87e90df1f9d48eea30d033
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-14 15:25:41 -0500

    Welcome Screen

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Segment, Image } from 'semantic-ui-react'
 
 import { connect } from 'react-redux';
 
@@ -8,9 +9,22 @@ class Welcome extends React.Component{
     render() {
         return(
             <div className='welcome-text'>
-                {/* Welcome {this.props.user.firstName ? this.props.user.firstName : 'Please Log In or Sign Up!'} */}
-                <h1>Welcome to StarCrosser</h1>
-                <p>The premier dating app for finding your match with the help of the ancient science of Astrology!</p>
+                <Segment color='grey'>
+                    {/* Welcome {this.props.user.firstName ? this.props.user.firstName : 'Please Log In or Sign Up!'} */}
+                    <h1>Welcome to StarCrosser</h1>
+                    <p>The premier dating app for finding your match with the help of the ancient science of Astrology!</p>
+                    <Image src="https://66.media.tumblr.com/5654154dba1ade64c67b682c5dee899b/cdf8fc99c7c5816d-aa/s500x750/c67670215fabdff021ddd526ac6477ecf5d08953.gif" fluid/>
+                    <h2>The process:</h2>
+                    <p>When you sign up you will input your birthday, and our team of space wizards will divinate your zodiac sign.</p>
+                    <p>You can check My Sign to see information about what your sign says about you, as well as what your compatable signs are.</p>
+                    <p>Check out explore to look for other users.</p>
+                    <p>You can filter through users by age, distance, and, of course, sign compatability.</p>
+                    <h2>Messaging Policy:</h2>
+                    <p>Once you've found someone you're interested in you can like and then message them.</p>
+                    <p>Just know this: you can only message someone once unless they like you back. So you better make that first message a good one!</p>
+                    <h1>Now Good Luck Finding Love Among The Stars!</h1>
+                    <Image src="https://66.media.tumblr.com/1504e8197104db630c1acb52dd8b8d89/tumblr_oln24vuJxT1txe8seo1_500.gif" fluid/>
+                </Segment>
             </div>
         )
     }
